debc5d6e9a00240a747ae57d4f270455f001128e
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2018-02-06 17:19:57 -0500

    Add users view function and template
    Also updated Header with new links

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from models import User
 
 
 ###
@@ -23,6 +24,12 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/users')
+def users():
+    users = User.query.all()
+
+    return render_template('users.html', users=users)
 
 
 ###
