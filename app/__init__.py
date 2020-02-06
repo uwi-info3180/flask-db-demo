5c122275d4f0f80d909259a96de6ae7268c7e7a8
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2020-02-05 22:22:16 -0500

    Add SECRET_KEY config

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Som3$ec5etK*y'  # Ideally you'd want this to be a much longer and more random string
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yourusername:yourpassword@localhost/databasename'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
 db = SQLAlchemy(app)
