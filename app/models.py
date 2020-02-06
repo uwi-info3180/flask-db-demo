1bd9b2ffb0816456519e7648defef21e859ba773
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2020-02-05 21:40:22 -0500

    Add example of how to set your own table name

--- app/models.py
+++ app/models.py
@@ -2,6 +2,9 @@ from . import db
 
 
 class User(db.Model):
+    # Uncomment the line below if you want to set your own table name
+    # __tablename__ = "user_profiles"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
