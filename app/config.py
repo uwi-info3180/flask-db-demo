c017e425efef128cde587f15736e896918beda33
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2021-02-10 23:33:54 -0500

    Add config file

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config(object):
+    """Base Config Object"""
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'postgresql://yourusername:yourpassword@localhost/databasename'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+
+class DevelopmentConfig(Config):
+    """Development Config that extends the Base Config Object"""
+    DEVELOPMENT = True
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Production Config that extends the Base Config Object"""
+    DEBUG = False
