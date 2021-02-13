e7f0782af2be0ed884c801df4b1be04d08b7cb99
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2021-02-13 16:33:22 -0500

    add comment to explain flask_script Manager

--- manage.py
+++ manage.py
@@ -7,8 +7,8 @@ from app import app # we import the app object from the app module
 from app import db
 
 migrate = Migrate(app, db)
-manager = Manager(app)
+manager = Manager(app) # keeps track of all the commands and handles how they are called from the command line
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    manager.run()
+    manager.run() # prepares your Manager instance to receive input from the command line.
