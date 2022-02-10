00545ea9a6cf6d1e0cad4863bc9cdc0cca042d41
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-02-09 23:29:39 -0500

    simplify config file and add option to load .env files if present

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config(object):
     """Base Config Object"""
@@ -6,12 +9,3 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://yourusername:yourpassword@localhost/databasename'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
-
-class DevelopmentConfig(Config):
-    """Development Config that extends the Base Config Object"""
-    DEVELOPMENT = True
-    DEBUG = True
-
-class ProductionConfig(Config):
-    """Production Config that extends the Base Config Object"""
-    DEBUG = False
