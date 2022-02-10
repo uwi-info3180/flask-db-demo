e107be341c07d7ab5b0fbdf631beb9664d4989d8
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-02-10 00:37:15 -0500

    Add fallbacks to os.environ.get()

--- app/config.py
+++ app/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://yourusername:yourpassword@localhost/databasename'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://yourusername:yourpassword@localhost/databasename')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
