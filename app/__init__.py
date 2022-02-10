6dccedf672524ef6aba90241795159950dbff9b7
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-02-10 00:30:51 -0500

    Use Flask CLI instead of separate script file for migrations
    This is due to the fact that flask_script is no longer being maintained.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .config import Config
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'Som3$ec5etK*y'  # Ideally you'd want this to be a much longer and more random string
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yourusername:yourpassword@localhost/databasename'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ylynfatt@localhost/ylynfatt'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
 
 # We can also import our config from an object in a separate file.
@@ -13,5 +14,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import views, models
