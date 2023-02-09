18217ae15a607aaf80118c973b794e0a153db407
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2023-02-08 22:18:46 -0500

    Remove some files that are no longer needed

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: flask db upgrade --directory migrations
-web: gunicorn -w 4 -b "0.0.0.0:$PORT" app:app
--- run.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python
-from app import app
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)
